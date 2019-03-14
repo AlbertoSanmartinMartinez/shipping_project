b56bccd1a10fdcdbc1197374f1c90b9e59101515
Alberto <juanbone16@gmail.com>
2019-03-14 23:47:59 +0100

    admin models done

--- shipping_project/shipping_app/admin.py
+++ shipping_project/shipping_app/admin.py
@@ -1,3 +1,21 @@
+
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+
 from django.contrib import admin
 
+from shipping_app import models as shipping_models
+
+
+admin.site.site_header = 'Shipping Project Django Admin'
+admin.autodiscover()
+
 # Register your models here.
+admin.site.register(shipping_models.Zone)
+admin.site.register(shipping_models.Shipper)
+admin.site.register(shipping_models.Constraint)
+admin.site.register(shipping_models.Method)
+
+admin.site.register(shipping_models.Region)
+admin.site.register(shipping_models.Province)
+admin.site.register(shipping_models.Country)
